b809b0daf30f1f92102db76306b44e20dc93a4ff
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-21 23:21:44 +0530

    Use GET POST PUT DELETE

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/bookRoute.js";
+
+const app = express();
+
+// MiddleWare for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
